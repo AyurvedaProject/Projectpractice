658b91228744c568a178d267ad802d1ab218b3ef
rajsinghthakure <rajthakur8827142011@gmail.com>
2024-04-04 17:07:01 +0530

    add array file

---
+++ sandeep/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3, 4, 5, 6 };
+        for (int i = 0; i <= a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
