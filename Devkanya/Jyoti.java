47a8aeb7b2d76fcdfc86b62feda2570d3205c37a
Jyoti Birla <jbirla998@gmail.com>
2024-04-04 16:54:40 +0530

    Edit

---
+++ Devkanya/Jyoti.java
@@ -0,0 +1,7 @@
+package Devkanya;
+
+public class Jyoti {
+    public static void main(String[] args) {
+        System.out.println("Jyoti");
+    }
+}
---
+++ advance-01/Jyoti.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class Jyoti {
+    public static void main(String[] args) {
+
+        System.out.println("Jyoti");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the nunmber1");
+        int n1=sc.nextInt();
+        System.out.println("Enter the number2");
+        int n2=sc.nextInt();
+        System.out.println(n1+n2);
+        
+
+    }
+}
