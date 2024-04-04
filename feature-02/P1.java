3e11ee8dd979182ba1f81ab8ad5bc47eb5cd1e14
DevkanyaLavriya <devkanyabirla8@gmail.com>
2024-04-04 16:54:48 +0530

    Editcode

--- feature-02/P1.java
+++ feature-02/P1.java
@@ -1,10 +1,7 @@
-package feature-02;
-
 public class P1 {
     public static void main(String[] args) {
         System.out.println("Hello world in feature - 02");
         System.out.println("Add line inside feature-01");
         System.out.println("Add line in feature-02 branch");
-
     }
 }
